eeb452a03972e81d5ac98b8819cf56c5de7e896f
seokjoon <seokjoon@gmail.com>
2023-09-22 18:18:22 +0900

    rename

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import './assets/main.css'
+import './asset/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
--- src/view/HomeView.vue
+++ src/view/HomeView.vue
@@ -1,5 +1,5 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import TheWelcome from '../component/TheWelcome.vue'
 </script>
 
 <template>
